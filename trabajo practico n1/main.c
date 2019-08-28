091439ee5907401d449c162ef71f1b1078f37a11
Ivana k <52494488+ivanakruk@users.noreply.github.com>
2019-08-28 12:23:23 -0300

    Add files via upload

--- trabajo practico n1/main.c
+++ trabajo practico n1/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "operaciones.h"
+
+int sumar (int num1, int num2);
+int restar (int num1, int num2);
+int multiplicar (int num1,int num2);
+float dividir (float num1, float num2);
+int factorial (int num1, int num2);
 
 int main()
 {
@@ -21,17 +28,43 @@ int main()
         fflush(stdin);
         scanf("%d", & b);
 
-        switch(operaciones)
+        printf("Seleccione la operacion que quiera realizar: ");
+        fflush(stdin);
+        printf("\n_________________________________________\n");
+
+        printf("1) Sumar \n");
+        fflush(stdin);
+        printf("2) Restar \n");
+        fflush(stdin);
+        printf("3) Multiplicar \n ");
+        fflush(stdin);
+        printf("4) Dividir \n");
+        fflush(stdin);
+        printf("5) Factorial \n");
+
+        scanf("%c",&operaciones);
+
+
+
+printf("El resultado es: %d",resultado);
+
+        printf("Desea salir?\n");
+        fflush(stdin);
+        scanf("%s", &seguir);
+
+    }while(seguir=='n');
+
+    switch(operaciones)
         {
         case '1':
-            resultado=a*b;
+            resultado=sumar(a,b);
             break;
         case '2':
-            resultado=a+b;
+            resultado=a-b;
             break;
 
         case '3':
-            resultado=a-b;
+            resultado=a*b;
             break;
         case '4':
             resultado=(float)a/b;
@@ -39,14 +72,25 @@ int main()
 
 
         }
-
-
-
-        printf("Desea continuar?\n");
-        fflush(stdin);
-        scanf("%s", &seguir);
-
-    }while(seguir=='s');
-
     return 0;
 }
+
+int sumar(int num1, int num2)
+{
+    int total;
+    total=num1+num2;
+    return total;
+}
+int restar(int num1, int num2)
+{
+    int total;
+    total=num1-num2;
+    return total;
+}
+int multiplicar(int num1, int num2)
+{
+    int total;
+    total=num1*num2;
+    return total;
+}
+
