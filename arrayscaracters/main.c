a818c1fd368488bf7848f8b3c7d52d7cc96d648d
unknown <ivana.kruk@gmail.com>
2019-09-16 21:12:39 -0300

    ejercicio del sab completado

--- arrayscaracters/main.c
+++ arrayscaracters/main.c
@@ -6,18 +6,19 @@
 
 void CargarNumerosTest(int array[],int limite);
 void MostrarNumerosTest(int array[],int limite);
-void cargarPalabraTest(char array [][50],int limite);
-void mostrarPalabrasTest(char array[][50],int limite);
-void cargarApellidoTes(char array[][50],int limite);
+void cargarNombre(char array [][50],int limite);
+void mostrarNombre(char array[][50],int limite);
+void cargarApellido(char array[][50],int limite);
+void mostrarApellidos(char array[][50],int limite);
 void ordenarPorEdades(char nombres[][50], char apellidos[][50],int edades[],int limite);
 void listar(char nombres[][50], char apellidos[][50],int edades[],int limite);
+void espacioparaSeparar();
 
 
 int main()
 {
     int datosNumericos[T];
-    char letra;
-    char palabra[T];
+    char Nombre[T][50];
     char ListadodePalabras[T][50];
 
    // CargarArray(datosNumericos,T,"ingrese un num","error ingrese num entre 0-200",0,200,2);
@@ -26,10 +27,13 @@ int main()
     CargarNumerosTest(datosNumericos,T);
     MostrarNumerosTest(datosNumericos,T);
 
-    cargarPalabraTest(ListadodePalabras,T);
-    mostrarPalabrasTest(ListadodePalabras,T);
-    cargarApellidoTes(ListadodePalabras,T);
-    mostrarApellidosTest(ListadodePalabras,T);
+    cargarNombre(Nombre,T);
+    mostrarNombre(Nombre,T);
+    cargarApellido(ListadodePalabras,T);
+    mostrarApellidos(ListadodePalabras,T);
+    espacioparaSeparar();
+    ordenarPorEdades(ListadodePalabras,ListadodePalabras,datosNumericos,T);
+    listar(Nombre,ListadodePalabras,datosNumericos,T);
 
     //ordenarPorEdades()
 
@@ -52,7 +56,7 @@ void MostrarNumerosTest(int array[],int limite)
        printf("\n Edad: %d",array[i]);
     }
 }
-void cargarPalabraTest(char array [][50],int limite)
+void cargarNombre(char array [][50],int limite)
 {
     char aux [][50]={"Carlos","Jorge","Maria"};
     int i;
@@ -61,7 +65,7 @@ void cargarPalabraTest(char array [][50],int limite)
         strcpy(array[i],aux[i]);
     }
 }
-void mostrarPalabrasTest(char array[][50],int limite)
+void mostrarNombre(char array[][50],int limite)
 {
     int i;
     for(i=0; i<limite; i++)
@@ -69,7 +73,7 @@ void mostrarPalabrasTest(char array[][50],int limite)
         printf("\n Nombre: %s",array[i]);
     }
 }
-void cargarApellidoTes(char array[][50],int limite)
+void cargarApellido(char array[][50],int limite)
 {
     char aux [][50]={"Mercurio","Agua","McCartney"};
     int i;
@@ -78,7 +82,7 @@ void cargarApellidoTes(char array[][50],int limite)
         strcpy(array[i],aux[i]);
     }
 }
-void mostrarApellidosTest(char array[][50],int limite)
+void mostrarApellidos(char array[][50],int limite)
 {
     int i;
     for(i=0; i<limite; i++)
@@ -88,7 +92,12 @@ void mostrarApellidosTest(char array[][50],int limite)
 }
 void listar(char nombres[][50], char apellidos[][50],int edades[],int limite)
 {
-    printf("\n %d     %s       %s      ",edades, nombres, apellidos);
+    int i;
+    for(i=0;i<limite;i++)
+    {
+        printf("\n %s, %s, edad %d", apellidos[i],nombres[i],edades[i]);
+    }
+
 }
 
 void ordenarPorEdades(char nombres[][50], char apellidos[][50],int edades[],int limite)
@@ -96,8 +105,8 @@ void ordenarPorEdades(char nombres[][50], char apellidos[][50],int edades[],int 
     int i;
     int j;
     int aux;
-    char auxA;
-    char auxB;
+    char auxNombres[50];
+    char auxApellidos[50];
     for (i=0; i<limite-1;i++)
     {
         for (j=i+1; j<limite;j++)
@@ -108,16 +117,20 @@ void ordenarPorEdades(char nombres[][50], char apellidos[][50],int edades[],int 
                 edades[i]=edades[j];
                 edades[j]=aux;
 
-               /* auxA = nombres[i];
-                nombres[i]=nombres[j];
-                nombres[j]=auxA;
+                strcpy(auxNombres,nombres[i]);
+                strcpy(nombres[i],nombres[j]);
+                strcpy(nombres[j],auxNombres);
 
-                auxB = apellidos[i];
-                apellidos[i]=apellidos[j];
-                apellidos[j]=auxB;*/
-
+                strcpy(auxApellidos,apellidos[i]);
+                strcpy(apellidos[i],apellidos[j]);
+                strcpy(apellidos[j],auxApellidos);
 
             }
         }
     }
+}
+void espacioparaSeparar()
+{
+    printf("\n\n________________________");
+    printf("\n\n Apellidos y nombres ordenados por edad\n");
 }
